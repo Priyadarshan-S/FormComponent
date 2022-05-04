758b48bf6220350e38582867a4a4a624f74020da
Priyadarshan S <65020277+Priyan3005@users.noreply.github.com>
2022-05-04 15:49:41 +0530

    Add files via upload

---
+++ controller/FormViewController.java
@@ -0,0 +1,44 @@
+package main.java.com.yali.form.controller;
+
+import com.zoho.yali.conf.YConfig;
+import com.zoho.yali.conf.YConfigFactory;
+import com.zoho.yali.ds.DSContext;
+import com.zoho.yali.view.ViewContext;
+import com.zoho.yali.view.ViewController;
+import com.zoho.yali.view.ViewModel;
+import com.zoho.yali.view.ViewModelConstructionException;
+
+import main.java.com.yali.form.controller.helper.FieldChecks;
+import main.java.com.yali.form.controller.helper.Login;
+import main.java.com.yali.form.model.FormViewModel;
+
+public class FormViewController extends ViewController{
+
+    public FormViewController(YConfig uiControlConfig) {
+        super(uiControlConfig);
+    }
+
+    @Override
+    public ViewModel getViewModel(ViewContext viewContext, DSContext arg1) throws ViewModelConstructionException {
+        FormViewModel userConfig = new FormViewModel(viewContext,YConfigFactory.getConfig());
+
+
+        return getFormViewModel(viewContext,userConfig);
+    }
+
+    public FormViewModel getFormViewModel(ViewContext viewContext,FormViewModel userConfig) {
+        FormViewModel model = new FormViewModel(viewContext, uiControlConfig());
+        
+        Login login = new Login();
+        String role = login.getRole();
+        
+        model.getFieldList().forEach(field ->{
+            String fieldType = field.getString("fieldType");
+            FieldChecks check = new FieldChecks();
+            
+            
+        });
+        return model;
+    }
+    
+}
